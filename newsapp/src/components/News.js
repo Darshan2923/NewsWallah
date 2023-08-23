c48c34c146ede9a17370c8d0b979be4516d6c545
Darshan <darshanpatel2923@gmail.com>
2023-08-23 21:19:48 +0530

    Added a basic prototype of NewItems in News.js

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/News.css'
+import NewsItems from './NewsItems'
 
 const News = () => {
     return (
         <section id="news">
             <div className='news__container'>
                 <h1 className='headline'>NewsWallah-Top News In General</h1>
+                <NewsItems />
             </div>
         </section>
 
